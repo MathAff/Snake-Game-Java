613f5d6e45b8eee09f2980da04ba4d5729baf292
MathAff <matheusafensor@gmail.com>
2024-11-17 21:15:51 -0300

    adding icon

--- src/game/GameFrame.java
+++ src/game/GameFrame.java
@@ -1,12 +1,16 @@
 package game;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.util.Objects;
 
 public class GameFrame extends JFrame {
 
     GameFrame(){
         this.add(new GamePanel());
         this.setTitle("Snake");
+
+        ImageIcon icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/icon.png")));
+        this.setIconImage(icon.getImage());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
